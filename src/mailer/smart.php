be0fa5cb41e7900479c6347f649dd01f57b7bacb
Mike <mixakievchanen@gmail.com>
2022-02-23 04:26:42 +0200

    .

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -14,12 +14,12 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'mixakievchanen@gmail.com';                 // Наш логин
-$mail->Password = 'ncnvovsljqtqytqa';                           // Наш пароль от ящика
+$mail->Password = '';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
 
 $mail->setFrom('mixakievchanen@gmail.com', 'Pulse');   // От кого письмо 
-$mail->addAddress('masit88912@submic.com');     // Add a recipient
+$mail->addAddress('');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
